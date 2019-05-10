fc34748cc6a9afe19d0dbd6587fe710ade9152e1
Dauno Martínez <Dauno@C02P4ADYG3QC.group.on>
2019-05-10 02:12:30 -0400

    redis test

--- src/modules/redis/redis.js
+++ src/modules/redis/redis.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis')
 const { redis } = require('../../config');
 
-const redisClient = new Redis(redis);
+const redisClient = new Redis(process.env.REDIS_URL);
 
 module.exports = redisClient;
